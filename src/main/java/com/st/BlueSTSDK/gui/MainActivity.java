f77d00b05425d2dec9529e6a704ce733a1efa98c
Giovanni Visentini <giovanni.visentini@st.com>
2016-04-08 16:18:02 +0200

    imporve the splash screen

--- src/main/java/com/st/BlueSTSDK/gui/MainActivity.java
+++ src/main/java/com/st/BlueSTSDK/gui/MainActivity.java
@@ -21,36 +21,17 @@ public abstract class MainActivity extends AppCompatActivity {
      * The number of milliseconds to wait after
      * user interaction before hiding the system UI.
      */
-    private static final int AUTO_HIDE_DELAY_MILLIS = 2000;
+    private static final int AUTO_HIDE_DELAY_MILLIS = 1000;
     private static final String SPLASH_SCREEN_WAS_SHOWN = MainActivity.class.getCanonicalName()+"" +
             ".SplashWasShown";
     /**
      * Some older devices needs a small delay between UI widget updates
      * and a change of the status and navigation bar.
      */
-    private static final int UI_ANIMATION_DELAY = 300;
     private final Handler mHideHandler = new Handler();
-    private ImageView mSplashView;
 
-    private final Runnable mShowSplashPart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            mSplashView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
     private View mControlsView;
-    private final Runnable mShowContentPart2Runnable = new Runnable() {
+    private final Runnable mShowContentRunnable = new Runnable() {
         @Override
         public void run() {
             // Delayed display of UI elements
@@ -63,14 +44,6 @@ public abstract class MainActivity extends AppCompatActivity {
         }
     };
 
-
-    private final Runnable mShowContentRunnable = new Runnable() {
-        @Override
-        public void run() {
-            showContent();
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -78,26 +51,38 @@ public abstract class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mControlsView = findViewById(R.id.main_content_controls);
-        mSplashView = (ImageView)findViewById(R.id.splashScreen_content);
-
-        // Set up the user interaction to manually showContent or showSplashScreen the system UI.
-        mSplashView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showContent();
-            }
-        });
 
         TextView versionText = (TextView) findViewById(R.id.versionText);
         TextView appText = (TextView) findViewById(R.id.appNameText);
         //show the version using the data in the manifest
+        String version=null;
+        CharSequence appName=null;
+
         try {
             PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
-            versionText.append(pInfo.versionName);
-            appText.setText(getPackageManager().getApplicationLabel(pInfo.applicationInfo));
+            version = pInfo.versionName;
+            appName = getPackageManager().getApplicationLabel(pInfo.applicationInfo);
+            if(versionText!=null) {
+                String temp = pInfo.versionName;
+                if (temp != null)
+                    versionText.append(temp);
+                else
+                    versionText.setText("");
+            }//if
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }//try-catch
+
+        if(versionText!=null){
+            if(version!=null){
+                versionText.append(version);
+            }else
+                versionText.setText("");
+        }
+
+        if(appText!=null && appName!=null){
+            appText.setText(appName);
+        }
 
     }
 
@@ -134,26 +119,13 @@ public abstract class MainActivity extends AppCompatActivity {
         mControlsView.setVisibility(View.GONE);
 
         // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowContentPart2Runnable);
-        mHideHandler.postDelayed(mShowSplashPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void showContent() {
-        // Show the system bar
-        mSplashView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mShowSplashPart2Runnable);
-        mHideHandler.postDelayed(mShowContentPart2Runnable, UI_ANIMATION_DELAY);
+        mHideHandler.removeCallbacks(mShowContentRunnable);
     }
 
     /**
      * Schedules a call to showContent() in [delay] milliseconds, canceling any
      * previously scheduled calls.
      */
-
     private void delayedShowContent(int delayMillis) {
         mHideHandler.removeCallbacks(mShowContentRunnable);
         mHideHandler.postDelayed(mShowContentRunnable, delayMillis);
