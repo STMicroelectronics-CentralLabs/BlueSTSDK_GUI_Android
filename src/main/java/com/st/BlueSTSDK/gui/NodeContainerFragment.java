c72d2bb9a2906d2ffc06c8aa0a3396b77cda8b2b
Giovanni Visentini <giovanni.visentini@st.com>
2016-11-10 14:20:08 +0100

    add license header

--- src/main/java/com/st/BlueSTSDK/gui/NodeContainerFragment.java
+++ src/main/java/com/st/BlueSTSDK/gui/NodeContainerFragment.java
@@ -1,3 +1,29 @@
+/*******************************************************************************
+ * COPYRIGHT(c) 2016 STMicroelectronics
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *   1. Redistributions of source code must retain the above copyright notice,
+ *      this list of conditions and the following disclaimer.
+ *   2. Redistributions in binary form must reproduce the above copyright notice,
+ *      this list of conditions and the following disclaimer in the documentation
+ *      and/or other materials provided with the distribution.
+ *   3. Neither the name of STMicroelectronics nor the names of its contributors
+ *      may be used to endorse or promote products derived from this software
+ *      without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************/
 package com.st.BlueSTSDK.gui;
 
 import android.annotation.TargetApi;
@@ -22,7 +48,7 @@ import com.st.BlueSTSDK.Node;
  * this class will start the connection with the node inside the onCreate and close it inside the
  * onDestroy.
  * If you move in nother activity that will use the same node you can avoid to disconnect calling
- * the method {@link NodeContainerFragment#keepConnectionOpen(boolean)}
+ * the method {@link NodeContainerFragment#keepConnectionOpen(boolean, boolean)}
  */
 public class NodeContainerFragment extends Fragment implements NodeContainer {
     private final static String TAG = NodeContainerFragment.class.getCanonicalName();
